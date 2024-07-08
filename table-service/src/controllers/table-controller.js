791765f37446630a5959b4a8062fb3c145a95d8c
Ly Tuan An <52000620@student.tdtu.edu.vn>
2024-07-08 21:34:10 +0700

    updated validator in table api

--- table-service/src/controllers/table-controller.js
+++ table-service/src/controllers/table-controller.js
@@ -1,34 +1,29 @@
 const createError = require('http-errors');
 const TableService = require('../services/table-service');
-const { validationResult, check } = require('express-validator');
-
-function validate(req, res, next) {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        const errorMessages = errors.array().map(error => ({ field: error.path, msg: error.msg }));
-        return res.status(400).json({ success: false, message: errorMessages, data: {} });
-    }
-    next();
-}
+const { check } = require('express-validator');
+const validator = require('../middlewares/input-validator');
 
 module.exports = {
     createTable: [
         check('no')
-            .not().isEmpty().withMessage('Table name cannot be empty.')
-            .isString().withMessage('Table name must be a string.'),
+            .notEmpty().withMessage('Table no cannot be empty')
+            .isString().withMessage('Table no must be a string'),
         check('capacity')
-            .not().isEmpty().withMessage('Capacity cannot be empty.')
-            .isInt({ min: 1 }).withMessage('Capacity must be a positive integer.'),
+            .notEmpty().withMessage('Capacity cannot be empty')
+            .isInt({ min: 1 }).withMessage('Capacity must be an integer > 0'),
         check('isVip')
-            .not().isEmpty().withMessage('VIP status cannot be empty.')
-            .isBoolean().withMessage('VIP status must be a boolean.'),
+            .optional()
+            .notEmpty().withMessage('VIP status cannot be empty')
+            .isBoolean().withMessage('VIP status must be a boolean'),
         check('status')
-            .not().isEmpty().withMessage('Status cannot be empty.')
-            .isIn(['free', 'occupied', 'reserved']).withMessage('Status must be either free, occupied, or reserved.'),
+            .optional()
+            .notEmpty().withMessage('Status cannot be empty')
+            .isIn(['free', 'occupied', 'reserved']).withMessage('Status must be either free, occupied, or reserved'),
         check('active')
-            .not().isEmpty().withMessage('Active status cannot be empty.')
-            .isBoolean().withMessage('Active status must be a boolean.'),
-        validate,
+            .optional()
+            .notEmpty().withMessage('Active status cannot be empty')
+            .isBoolean().withMessage('Active status must be a boolean'),
+        validator,
         async (req, res, next) => {
             try {
                 const { no, capacity, isVip, status, active } = req.body;
@@ -36,7 +31,7 @@ module.exports = {
                 return res.status(201).json({
                     success: true,
                     message: 'Create table sucessfully!',
-                    data: { newTable }
+                    data: { table: newTable }
                 });
             } catch (error) {
                 next(error);
@@ -74,45 +69,46 @@ module.exports = {
 
     // Update table by ID
     updateTable: [
-        check('id')
-            .not().isEmpty().withMessage('ID cannot be empty.')
-            .isInt({ min: 1 }).withMessage('ID must be a positive integer.'),
         check('no')
-            .not().isEmpty().withMessage('Table name cannot be empty.')
-            .isString().withMessage('Table name must be a string.'),
+            .optional()
+            .notEmpty().withMessage('Table no cannot be empty.')
+            .isString().withMessage('Table no must be a string.'),
         check('capacity')
-            .not().isEmpty().withMessage('Capacity cannot be empty.')
+            .optional()
+            .notEmpty().withMessage('Capacity cannot be empty.')
             .isInt({ min: 1 }).withMessage('Capacity must be a positive integer.'),
-        check('isVip')
-            .not().isEmpty().withMessage('VIP status cannot be empty.')
+        check('isVip').optional()
+            .notEmpty().withMessage('VIP status cannot be empty.')
             .isBoolean().withMessage('VIP status must be a boolean.'),
         check('status')
-            .not().isEmpty().withMessage('Status cannot be empty.')
+            .optional()
+            .notEmpty().withMessage('Status cannot be empty.')
             .isIn(['free', 'occupied', 'reserved']).withMessage('Status must be either free, occupied, or reserved.'),
         check('active')
-            .not().isEmpty().withMessage('Active status cannot be empty.')
+            .optional()
+            .notEmpty().withMessage('Active status cannot be empty.')
             .isBoolean().withMessage('Active status must be a boolean.'),
-        validate,
+        validator,
         async (req, res, next) => {
             try {
                 const { id } = req.params;
                 const { no, capacity, isVip, status, active } = req.body;
-                const updatedTable = await TableService.updateTable({id, no, capacity, isVip, status, active});
+                const updatedTable = await TableService.updateTable({ id, no, capacity, isVip, status, active });
                 if (updatedTable) {
                     return res.status(200).json({
                         sucess: true,
                         message: 'Update table successfully!',
-                        data: { updatedTable }
+                        data: { table: updatedTable }
                     });
                 } else {
-                    return res.status(404).json({ sucess: false, error: { message: 'Table not found', data: {} } });
+                    return next(createError(404, 'Table not found'));
                 }
             } catch (error) {
                 next(error);
             }
         }
     ],
-      
+
     // Delete table by ID
     async deleteTable(req, res, next) {
         try {
